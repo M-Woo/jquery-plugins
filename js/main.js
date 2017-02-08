9918c9c7897b2b58178507d9e233b625f5a29559
m-woo <mark.chi.woo@gmail.com>
2017-02-08 11:00:34 -0800

    HTML/Style/JQuery effects

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log('hi')
+
+var tabs = $('#tabs')
+
+
+$(function() {
+  tabs.tabs();
+});
+
+$( function() {
+  tabs.accordion();
+});
+
+$('h1').parallax({imageSrc: 'adorable-little-rose-cupcakes.jpg'})
+$('#Image').parallax({imageSrc: 'adorable-little-rose-cupcakes.jpg'})
+$('#tabs-1').parallax({imageSrc: 'adorable-little-rose-cupcakes.jpg'})
